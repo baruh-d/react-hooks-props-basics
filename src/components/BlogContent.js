3e37210126edb750ea0f4c44794bc99453fd9f23
baruh-d <barudaniel6@gmail.com>
2024-02-01 01:01:12 +0300

    props

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if(!props.isPublished){
+    return null;
+  } else{
+    return (
+      <div id="blog-content">{props.articleText}
+        <h1>{props.isPublished}</h1>
+        <p>minutesToRead: {props.minutesToRead} </p>
+      </div>
+    )
+  }
 }
 
 export default BlogContent;
